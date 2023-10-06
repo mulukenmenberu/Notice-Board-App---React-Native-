1892626106edbafea5a8e6b0967c1aa7b9ad0250
Muluken <mulerapps@gmail.com>
2023-10-06 17:15:50 +0300

    up improvments

--- components/Login.js
+++ components/Login.js
@@ -9,15 +9,17 @@ export default function Login({navigation}) {
     <ImageBackground source={require('../assets/image.png')} resizeMode='cover'>
     <SafeAreaView style={styles.container}>
  
-      
+    <Image source={require('../assets/logo.png')}
+style={{width:150,height:150, alignSelf:'center', marginTop:-180}} />
+
       
 <Text style={{justifyContent:'center', alignItems:'center', fontSize:20, alignSelf:'center'}}>
            Type your password</Text>
            <TextInput 
            secureTextEntry
-           style={{borderRadius:20,marginTop:10,alignSelf:'center',height:40, width:250, borderWidth:3, borderColor:'pink'}}
+           style={{borderRadius:15,marginTop:10,alignSelf:'center',height:60, paddingLeft:30,width:250, borderWidth:3, borderColor:'pink'}}
            />
-           <Text style={{marginTop:20, justifyContent:'center', alignItems:'center', fontSize:20, alignSelf:'center'}}>
+           <Text style={{marginTop:15, justifyContent:'center', alignItems:'center', fontSize:20, alignSelf:'center'}}>
            forgot password?</Text>
 <TouchableOpacity onPress={()=>navigation.navigate('Tabs')} style={{borderRadius:20,alignSelf:'center',alignItems:'center',width:200, height:50, color:'#fff', backgroundColor:'#087AFC', marginTop:5, justifyContent:'center'}}>
 <Text style={{justifyContent:'center', fontSize:20,color:'#fff'}}>Sign In</Text>
--- components/Saved.js
+++ components/Saved.js
@@ -1,6 +1,6 @@
 // import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { Image, StatusBar,TextInput, StyleSheet, Text, View, Platform, StatusBar as stbar, Dimensions, SafeAreaView, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
+import { Image, StatusBar, TextInput, StyleSheet, Text, View, Platform, StatusBar as stbar, Dimensions, SafeAreaView, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
 import { Card } from 'react-native-paper';
 
 
@@ -18,23 +18,34 @@ const hheight = Dimensions.get('screen').height
 export default function Saved({ navigation }) {
     const { width, height } = Dimensions.get('screen')
     const [active, setActive] = useState(0)
-    useEffect(()=>{
-        latest.map((item)=>{
+    useEffect(() => {
+        latest.map((item) => {
             console.log(item.content)
 
         })
-    },[])
+    }, [])
+    const measureTextHeight = (text, fontSize, width) => {
+        const numberOfLines = 3.5; // Define the maximum number of lines
+        const lineHeight = fontSize * 1.2; // Adjust this value as needed
+        const maxHeight = lineHeight * numberOfLines;
+
+        const TextHeight = Math.ceil(text.length / (width / fontSize)) * lineHeight;
+
+        return TextHeight <= maxHeight;
+    };
+
     return (
         <SafeAreaView style={styles.container}>
+
+            <View style={{ marginLeft: 10, marginTop: 10, marginRight: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Image source={require('../assets/logo.png')} style={{ width: 30, height: 30 }} />
+                <Text style={{ fontWeight: 'bold' }}>Dashboard</Text>
+                {/* <Text style={{ fontWeight: 'bold' }}>Search</Text> */}
+                {/* <EvilIcons name="search" size={24} color="black" /> */}
+                <AntDesign name="search1" size={24} color="black" />
+            </View>
             <ScrollView
             >
-                <View style={{ marginLeft: 10, marginTop:10,marginRight: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <Image source={require('../assets/logo.png')} style={{ width: 30, height: 30 }} />
-                    <Text style={{ fontWeight: 'bold' }}>Dashboard</Text>
-                    {/* <Text style={{ fontWeight: 'bold' }}>Search</Text> */}
-                    {/* <EvilIcons name="search" size={24} color="black" /> */}
-                    <AntDesign name="search1" size={24} color="black" />
-                </View>
                 <Card style={{ marginTop: 10, alignSelf: 'center', height: 80, width: width - 20, backgroundColor: '#F0F3F7', justifyContent: 'center' }}>
                     <View style={{ marginLeft: 10, marginRight: 10, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
 
@@ -42,9 +53,9 @@ export default function Saved({ navigation }) {
                             <Image source={require('../assets/mule.png')} style={{ width: 50, height: 50, borderRadius: 50 / 2 }} />
                         </View>
                         <View style={{ marginLeft: 20 }}>
-                            <View style={{flexDirection:'row'}}>
-                            <Text style={{ color: 'tomato', fontWeight: 'bold', fontSize: 19 }}>Muluken M </Text>
-                            <AntDesign name="edit" size={24} color="black" />
+                            <View style={{ flexDirection: 'row' }}>
+                                <Text style={{ color: 'tomato', fontWeight: 'bold', fontSize: 19 }}>Muluken M </Text>
+                                <AntDesign name="edit" size={24} color="black" />
                             </View>
                             <Text>Information Technology - 3rd year</Text>
                         </View>
@@ -54,7 +65,7 @@ export default function Saved({ navigation }) {
                 <Text style={{ marginLeft: 10, marginTop: 10, fontSize: 20 }}>
                     Saved Items</Text>
 
-                
+
                 <View>
                     <ScrollView
                         horizontal
@@ -69,42 +80,60 @@ export default function Saved({ navigation }) {
 
                 <View>
                     {
-                        latest.map((item)=>{
+                        latest.map((item) => {
+                            const isContentExpanded = measureTextHeight(item.content, 17, width - 40);
+
+                            const [isExpanded, setIsExpanded] = useState(false);
+
+                            const toggleReadMore = () => {
+                                setIsExpanded(!isExpanded);
+                            };
+                            const containerHeight = isExpanded ? 'auto' : isContentExpanded ? 'auto' : 200;
+
+
                             return (
-                                <View style={{ marginTop: 10, alignSelf: 'center', borderRadius: 20, backgroundColor: '#9FE2BF', height: 200, width: width - 40 }}>
-<View style={{flexDirection:'row', justifyContent:'space-evenly'}}>
-<Text style={{fontWeight:'bold',padding:20,color:'#222', fontSize:20, alignSelf:'flex-start'}}>{item.title}</Text>
-<AntDesign name="save" style={{padding:20}} size={16} color="#1B54DB" />
+                                <View style={{ marginTop: 15, alignSelf: 'center', borderRadius: 20, backgroundColor: '#fff', height: containerHeight, width: width - 40 }}>
+                                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                                        <Text style={{ fontWeight: 'bold', padding: 20, color: '#222', fontSize: 20, alignSelf: 'flex-start' }}>{item.title}</Text>
+                                        <AntDesign name="save" style={{ padding: 20 }} size={16} color="#1B54DB" />
 
-</View>
-<Text style={{paddingLeft:10,paddingRight:10,color:'#222', fontSize:17, alignSelf:'flex-start'}}>{item.content}</Text>
-                     
-                        <View style={{  flex: 1,
-  justifyContent: 'flex-end',
-  flexDirection:'column'}}>
-    <View style={{padding:20,paddingBotom:30,flexDirection:'row', justifyContent:'space-evenly'}}>
-    <AntDesign name="like1" size={16} color="#1B54DB" >
-    <Text> {item.likes}</Text>
-    </AntDesign>
+                                    </View>
+                                    {/* <Text style={{paddingLeft:10,paddingRight:10,color:'#222', fontSize:17, alignSelf:'flex-start'}}>{item.content}</Text> */}
+                                    <Text style={{ paddingLeft: 10, paddingRight: 10, color: '#222', fontSize: 17, alignSelf: 'flex-start' }}>
+                                        {isExpanded ? item.content : `${item.content.substring(0, 3.5 * (width - 40) / 17)}... `}
+                                        {!isExpanded && (
+                                            <Text onPress={toggleReadMore} style={{ color: '#1B54DB', alignSelf: 'flex-start' }}>Read More</Text>
+                                        )}
+                                    </Text>
 
-    <AntDesign name="eye" size={16} color="black" >
-        <Text> {item.seen}</Text>
-    </AntDesign>
-    {/* <AntDesign name="like1" size={16} color="#fff" > */}
-    <FontAwesome name="commenting-o" size={16} color="black" >
-    <Text> {item.comments}</Text>
-    </FontAwesome>
+                                    <View style={{
+                                        flex: 1,
+                                        justifyContent: 'flex-end',
+                                        flexDirection: 'column'
+                                    }}>
+                                        <View style={{ padding: 20, paddingBotom: 30, flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                                            <AntDesign name="like1" size={16} color="#1B54DB" >
+                                                <Text> {item.likes}</Text>
+                                            </AntDesign>
 
-                            
-                           
-                            </View>
-                        </View>
-                        </View>
+                                            <AntDesign name="eye" size={16} color="black" >
+                                                <Text> {item.seen}</Text>
+                                            </AntDesign>
+                                            {/* <AntDesign name="like1" size={16} color="#fff" > */}
+                                            <FontAwesome name="commenting-o" size={16} color="black" >
+                                                <Text> {item.comments}</Text>
+                                            </FontAwesome>
+
+
+
+                                        </View>
+                                    </View>
+                                </View>
                             )
-                    })
-                }
-                   
-                  
+                        })
+                    }
+
+
                 </View>
             </ScrollView>
             <StatusBar style="auto" />
